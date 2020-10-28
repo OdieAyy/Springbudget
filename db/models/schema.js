7203877ff31111d2476a38a4cc096d3679544056
adrian <adrian.v8@icloud.com>
2020-10-28 21:19:40 +0200

    Updated data base too use more logical schemas

--- db/models/schema.js
+++ db/models/schema.js
@@ -1,16 +1,59 @@
-const mongoose = require('mongoose')
+const { Schema } = require('mongoose')
+const db = require('../database')
 
-const User = new mongoose.Schema({
-    uname: String,
-    email: String,
-    salary: Number, 
+const User = new Schema({
+    username: {
+        type: String, 
+        required: true
+    }, 
+    email: {
+        type: String, 
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }, 
+    MonthlyIncome: {
+        type: Schema.Types.ObjectId,
+        ref: 'MonthlyIncome'
+    },
     expenses: [{
-        name: String,
-        cost: Number,
-        descipt: String
+        type: Schema.Types.ObjectId, 
+        ref: 'Expenses'
     }]
-})
+});
 
-const user = mongoose.model('user', User)
+const MonthlyIncome = new Schema({
+    type: {
+        name: String,
+        choices: ['Monthly', 'Weekly'],
+        required: true
+    },
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }
+});
 
-module.exports = user
+const Expenses = new Schema({
+    name: {
+        type: String, 
+        required: true,
+    },
+    frequency: {
+        type: String,
+        choices: ['Monthly', 'Weekly', 'Daily'],
+        required: true
+    }, 
+    description: {
+        type: String,
+        required: false
+    }
+});
+
+const UserModel = mongoose.model('user', User);
+const MonthlyIncomeModel = mongoose.model('monthlyIncome', MonthlyIncome);
+const ExpensesModel = mongoose.model('expenses', Expenses)
+
+module.exports = { UserModel, MonthlyIncomeModel, ExpensesModel }
